6bc4625d14fdedbb562bae9fe4f98b9f18933425
Mahler, Thomas <thomas.mahler@ista.com>
2021-11-08 11:31:30 +0100

    make it shine!

--- morphogrammatics/app/Main.hs
+++ morphogrammatics/app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
-import Lib
+import Kenogrammatics
 
 main :: IO ()
-main = someFunc
+main = do
+  print $ tnf "Hello World"
+  print $ tContexture 4
+  print $ kligate (tnf [1,2,3]) (tnf [1]) []
